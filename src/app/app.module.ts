d6ae09e1eb9e8e5754f4fccac9a51de0f6147630
Alvaro De León <orochies@gmail.com>
2018-01-23 12:45:00 -0600

    removed - unnecessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSidenavModule } from '@angular/material';
-import { MatToolbarModule } from '@angular/material';
-import { MatButtonModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
